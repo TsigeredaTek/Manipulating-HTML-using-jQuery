541edb00ef3fc890cd35a0a05e385cca74a293db
TsigeredaTek <rose_ym@yahoo.com>
2020-09-24 14:15:15 -0500

    my jquery

---
+++ scripts.js
@@ -0,0 +1,53 @@
+//OBJECTIVE 1
+let $button = $("<button>Click Me!</button>");
+$button.appendTo("body");
+$button.click(function(){
+    alert("Hello there.")
+});
+
+//OBJECTIVE 2
+let $button = $("#button");
+$button.click(function(){
+    let message = $("#text-box").val();
+    alert(message);
+});
+
+//OBJECTIVE 3
+let $div = $("#div");
+$div.hover(function(){
+    $div.style.backgroundColor = "red"; 
+}, function () {
+    $div.style.backgroundColor = "black";
+});
+
+//OBJECTIVE 4
+let $p = $("#p");
+$p.click(function(){
+    let $allchar = "0123456789ABCDEF";
+    let $randcol = "";
+
+    for (let i = 0; i < 6; i++) {
+        $randcol += $allchar[Math.floor(Math.random() *16)];
+    }
+
+    $($p).css("color", $randcol);
+});
+
+//OBJECTIVE 5
+let $button = $("#button");
+let $div = $("#div");
+$button.click(function(){
+    let $span = $("<spa>Rose</span>");
+    $($span).appendTo($div);
+});
+
+
+let friends = ["Biki", "Mizu", "Tsion", "Semru", "Agugu", "Selam", "Geni", "Azeb", "Hiwi", "Chuni"];
+let $button = $("#button");
+let $ul = $("#ul");
+$($button).click(function () {
+    for (let i = 0; i < $friends.length; i++) {
+        let $li = $("<li>" + ($friends[i]) + "</li>");
+        $($li).appendTo($ul);
+    }
+});
